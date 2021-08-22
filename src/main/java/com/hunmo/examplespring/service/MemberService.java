36b17e1ee62e6d7120647976d7aed19df7c99b05
hunmoyang <didgnsah7320@gmail.com>
2021-08-22 23:29:43 +0900

    1. MemberService 추가 2. MemberServiceTest 추가

---
+++ src/main/java/com/hunmo/examplespring/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.hunmo.examplespring.service;
+
+import com.hunmo.examplespring.domain.Member;
+import com.hunmo.examplespring.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//service는 비즈니스에 의존적 설계, repository는 기계적으로 작동하게 만듬
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증 memberRepository.save(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+
+    /**
+     *전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
