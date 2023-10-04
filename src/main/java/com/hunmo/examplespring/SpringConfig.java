fc3e44e854f1da71c4440f6d61169f039f2e8e1c
ArdorHoon <didgnsah7320@gmail.com>
2023-10-04 11:12:20 +0900

    jdbc template

--- src/main/java/com/hunmo/examplespring/SpringConfig.java
+++ src/main/java/com/hunmo/examplespring/SpringConfig.java
@@ -2,12 +2,23 @@ package com.hunmo.examplespring;
 
 import com.hunmo.examplespring.repository.MemberRepository;
 import com.hunmo.examplespring.repository.MemoryMemberRepository;
+import com.hunmo.examplespring.repository.jdbcTemplateMemberRepository;
 import com.hunmo.examplespring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new jdbcTemplateMemberRepository(dataSource);
     }
 }
